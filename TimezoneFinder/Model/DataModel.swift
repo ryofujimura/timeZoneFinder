45be00b1226350c35f9527c8faed10a1f88e7fa7
Ryo Fujimura <ryo176999@gmail.com>
2025-04-22 13:03:56 -0700

    Add custom country name mappings for better display cont.

--- TimezoneFinder/Model/DataModel.swift
+++ TimezoneFinder/Model/DataModel.swift
@@ -160,16 +160,9 @@ class CitySearchViewModel: ObservableObject {
     let countryNameMappings: [String: String] = [
         "United States": "USA",
         "United Kingdom": "UK",
-        "Russian Federation": "Russia",
         "United Arab Emirates": "UAE",
-        "Korea, Republic of": "South Korea",
-        "Korea, Democratic People's Republic of": "North Korea",
-        "Viet Nam": "Vietnam",
-        "Lao People's Democratic Republic": "Laos",
-        "Hong Kong": "Hong Kong SAR",
-        "Australia": "AUS",
-        "China": "CN",
-        "Japan": "JP"
+        "China mainland": "China",
+        "Congo - Brazzaville": "Congo",
     ]
     
     // Preset emojis for their locations
